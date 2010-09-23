6c215bd1af770fc9f87f999af1749739213009c0
Remi Chaintron <remi.chaintron@gmail.com>
2010-09-23 22:38:56 +0200

    zsh
     * Defined colors as env variables for scripts

---
+++ zsh/zsh/colors.zsh
@@ -0,0 +1,16 @@
+# File: colors.zsh
+# Brief: Zsh colors configuration file
+# Version: 0.1
+# Author: Scyn - Remi Chaintron <remi.chaintron@gmail.com>
+
+blue="\033[34m"
+lblue="\033[34;1m"
+red="\033[31m"
+lred="\033[31;1m"
+yellow="\033[33m"
+lyellow="\033[33;1m"
+green="\033[32m"
+lgreen="\033[32;1m"
+rst="\033[0m"
+
+
