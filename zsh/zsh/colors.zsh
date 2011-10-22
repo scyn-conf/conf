f92c30586b644441e47beb77553246482f8f4c63
Remi Chaintron <remi.chaintron@gmail.com>
2011-10-22 16:00:55 +0200

    Define colors for zsh

---
+++ zsh/zsh/colors.zsh
@@ -0,0 +1,22 @@
+# File: colors.zsh
+# Brief: Zsh color variables definitions
+# Version: 0.1
+# Author: Scyn - Remi Chaintron <remi.chaintron@gmail.com>
+#
+color_reset="%{`print "\e(B\e)B\e*B\e+B"`%}"
+color_white="%{`print "\e[37;1m"`%}"
+color_lwhite="%{`print "\e[37m"`%}"
+color_yellow="%{`print "\e[33m"`%}"
+color_lyellow="%{`print "\e[33;1m"`%}"
+color_red="%{`print "\e[31m"`%}"
+color_lred="%{`print "\e[31;1m"`%}"
+color_green="%{`print "\e[32m"`%}"
+color_lgreen="%{`print "\e[32;1m"`%}"
+color_blue="%{`print "\e[34m"`%}"
+color_lblue="%{`print "\e[34;1m"`%}"
+color_purple="%{`print "\e[35m"`%}"
+color_lpurple="%{`print "\e[35;1m"`%}"
+color_cyan="%{`print "\e[36m"`%}" # cyan
+color_lcyan="%{`print "\e[36;1m"`%}"
+color_clr="%{$reset_color%}"
+
